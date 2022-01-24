c67c898adc40259fe281fe8b421b407c927c7648
raihanFathuM <3103120184@student.smktelkom-pwt.sch.id>
2022-01-24 20:51:38 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document | About</title>
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> Telkom School </h3>
+    <p> @stematelpwt </p>
+    <img src="images/logo ts.jpg" alt="TS" width="200px">
+</body>
+</html>
